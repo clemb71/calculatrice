b82d7ccb4aeda27287ca01db10c41c32812907af
clemb <baston.clement@gmail.com>
2020-05-10 15:22:56 -0400

    Parenthese OK; Prio OK; rendre plus propre?

--- calculatrice1/calculatrice1/calculus.cpp
+++ calculatrice1/calculatrice1/calculus.cpp
@@ -11,6 +11,35 @@
 #include <regex>
 #include "calculus.h"
 
+
+double Calculate(std::smatch matches)
+{
+	double firstOperand = std::stod(matches[1]);
+	double secondOperand = std::stod(matches[3]);
+	std::string operation = matches[2];
+
+	if (operation == "+")
+	{
+		return firstOperand + secondOperand; // Does an addition
+	}
+	else if (operation == "-")
+	{
+		return firstOperand - secondOperand; // Does a substraction
+	}
+	else if (operation == "*")
+	{
+		return firstOperand * secondOperand; // Does a multiplication
+	}
+	else if (operation == "/")
+	{
+		return firstOperand / secondOperand; // Does a division
+	}
+	else if (operation == "%")
+	{
+		return int(firstOperand) % int(secondOperand); // Does a modulo with the closest rounded down integer
+	}
+}
+
 std::string AskToWriteOperation()
 {
 	std::string operation; // operation to be calculated
@@ -26,58 +55,33 @@ double PriorityOp(std::string operation)
 {
 	std::regex sousOperation("[\\(]([^\\(\\)]*)[\\)]");
 	std::smatch matches;
-	double resultat =0;
+	double resultat = 0;
 
-	
+
 
 	if (std::regex_search(operation, matches, sousOperation))
 	{
-		resultat = PriorityOp(matches.prefix().str() + std::to_string(PriorityOp(matches[1]))+ matches.suffix().str()) ;
+		resultat = PriorityOp(matches.prefix().str() + std::to_string(PriorityOp(matches[1])) + matches.suffix().str());
 
 	}
 	else
 	{
 		double firstOperand, secondOperand; //operands for the calculation
-		
-		std::regex operationRegex("(\\d*\\.?\\d+)([\\+\\-\\/\\*])(\\d*\\.?\\d+)");
+
+		std::regex operationPrio1Regex("(\\d*\\.?\\d+)([\\/\\*])(\\d*\\.?\\d+)");
+		std::regex operationPrio2Regex("(\\d*\\.?\\d+)([\\+\\-])(\\d*\\.?\\d+)");
 		std::regex endOfOperationRegex("^(\\d*\\.?\\d*)$");
 
-		if (std::regex_search(operation, matches, operationRegex))
+
+		if (std::regex_search(operation, matches, operationPrio1Regex))
 		{
-			firstOperand = std::stod(matches[1]);
-			secondOperand = std::stod(matches[3]);
-			std::string operation = matches[2];
-			
-			if (operation == "+")
-			{
-				resultat = firstOperand + secondOperand; // Does an addition
-			}
-			else if (operation == "-")
-			{
-				resultat = firstOperand - secondOperand; // Does a substraction
-			}
-			else if (operation == "*")
-			{
-				resultat = firstOperand * secondOperand; // Does a multiplication
-			}
-			else if (operation == "/")
-			{
-				resultat = firstOperand / secondOperand; // Does a division
-			}
-			else if (operation == "%")
-			{
-				resultat = int(firstOperand) % int(secondOperand); // Does a modulo with the closest rounded down integer
-			}
-			else
-			{
-				std::cout << "Incorrect operation type. Please try again" << std::endl;
-			}
-			std::string newOp = std::to_string(resultat) + matches.suffix().str();
-			resultat = PriorityOp(newOp);
-			return resultat;
-			
+			resultat = Calculate(matches);
 		}
-		else if(std::regex_search(operation, matches, endOfOperationRegex))
+		else if (std::regex_search(operation, matches, operationPrio2Regex))
+		{
+			resultat = Calculate(matches);
+		}
+		else if (std::regex_search(operation, matches, endOfOperationRegex))
 		{
 			return std::stod(matches[1]);
 		}
@@ -85,9 +89,13 @@ double PriorityOp(std::string operation)
 		{
 			std::cout << "Incorrect operation type. Please try again" << std::endl;
 		}
-	
+		std::string newOp = matches.prefix().str() + std::to_string(resultat) + matches.suffix().str();
+		resultat = PriorityOp(newOp);
+		return resultat;
+
 	}
-	return resultat;
+
+		return resultat;
 }
 
 double GiveResultToDesiredOperation()
